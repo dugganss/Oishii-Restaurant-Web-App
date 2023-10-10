88e695beab9c463c07a1b986eb130e724192ff5d
samdu <samdu@SAMDUGGLAPTOP>
2023-10-10 16:09:10 +0100

    Add project files.

---
+++ CO5227_Assignment/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO5227_Assignment.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
