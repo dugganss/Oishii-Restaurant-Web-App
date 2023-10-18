6323740b7e5e148875fea2a3aaed619626cb5d1b
SAM DUGGAN <2228246@chester.ac.uk>
2023-10-18 19:05:49 +0100

    edited index page, finalised navbar

---
+++ CO5227_Assignment/Pages/Basket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO5227_Assignment.Pages
+{
+    public class BasketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
