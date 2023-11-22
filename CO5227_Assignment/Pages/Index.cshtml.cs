41e98278b8299e0877b5ab3fc12f523c6a7c3244
SAM DUGGAN <2228246@chester.ac.uk>
2023-11-22 17:52:02 +0000

    Added C# code to index page that will allow for me to display data from database on it.

--- CO5227_Assignment/Pages/Index.cshtml.cs
+++ CO5227_Assignment/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using CO5227_Assignment.Data;
+using CO5227_Assignment.wwwroot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CO5227_Assignment.Pages
 {
@@ -12,10 +14,16 @@ namespace CO5227_Assignment.Pages
             _logger = logger;
         }
 
-        private readonly CO5227_AssignmentContext _db;
+        private readonly CO5227_AssignmentContext _context;
+
+        public IndexModel(CO5227_AssignmentContext context) {
+        _context = context;
+        }
+
+        public IList<MenuItems> MenuItem { get; set; } = default!;
         public void OnGet()
         {
-
+            MenuItem = _context.MenuItemss.FromSqlRaw("Select * From MenuItems").ToList();
         }
     }
 }
